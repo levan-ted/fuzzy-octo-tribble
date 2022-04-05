d7f3dcd48b313e8b23e015bbcb14afcf7ef8bae3
Levan Tediashvili <levan.tediashvili@outlook.com>
2022-04-05 20:27:42 +0400

    feat: style cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,9 +4,10 @@ import classes from "./Card.module.scss";
 const Card = ({ data, className }) => {
   return (
     <div className={`${classes.card} ${className}`}>
-      <div>
+      <div className={classes["image-container"]}>
         <img className={classes.image} src={data.imagePath} alt={data.title} />
       </div>
+      <span className={classes.title}>{data.title}</span>
     </div>
   );
 };
